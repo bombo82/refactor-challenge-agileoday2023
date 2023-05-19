be013f65e95460375914c926465eb9a362756db1
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-19 15:50:01 +0200

    7

--- java/src/main/java/it/giannibombelli/refactor_challenge/FizzBuzz.java
+++ java/src/main/java/it/giannibombelli/refactor_challenge/FizzBuzz.java
@@ -1,14 +1,21 @@
 package it.giannibombelli.refactor_challenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FizzBuzz implements FizzBuzzInterface {
     @Override
     public String say(int number) {
         String accumulator = "";
 
         var fizzDivisorAndWord = new DivisorAndWord(3, "Fizz");
-        if (number % fizzDivisorAndWord.getDivisor() == 0) {
-            accumulator += fizzDivisorAndWord.getWord();
+        var divisorAndWordList = Arrays.asList(fizzDivisorAndWord);
+        for (var divisorAndWord : divisorAndWordList) {
+            if (number % divisorAndWord.getDivisor() == 0) {
+                accumulator += divisorAndWord.getWord();
+            }
         }
+
         var buzzDivisorAndWord = new DivisorAndWord(5, "Buzz");
         if (number % buzzDivisorAndWord.getDivisor() == 0) {
             accumulator += buzzDivisorAndWord.getWord();
