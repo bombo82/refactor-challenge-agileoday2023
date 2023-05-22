d93f3451d538742fdbe6451cae655391ec0c8632
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-22 12:59:15 +0200

    rimozione del parametro di default nel costruttore

--- kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
+++ kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
@@ -1,11 +1,6 @@
 package it.giannibombelli.refactor_challenge
 
-class FizzBuzz(
-    private val divisorAndWordList: List<DivisorAndWord> = listOf(
-        DivisorAndWord(3, "Fizz"),
-        DivisorAndWord(5, "Buzz")
-    )
-) : FizzBuzzInterface {
+class FizzBuzz(private val divisorAndWordList: List<DivisorAndWord>) : FizzBuzzInterface {
     override fun say(number: Int): String = divisorAndWordList
         .joinToString("") { it.getWordOrEmpty(number) }
         .ifEmpty { number.toString() }
