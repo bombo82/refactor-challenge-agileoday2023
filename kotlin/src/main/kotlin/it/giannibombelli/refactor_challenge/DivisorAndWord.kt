17bf06eb176504553119fbc658db4abf5c9e6086
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-22 12:52:08 +0200

    inline metodo

--- kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/DivisorAndWord.kt
+++ kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/DivisorAndWord.kt
@@ -1,10 +1,5 @@
 package it.giannibombelli.refactor_challenge
 
 class DivisorAndWord(private val divisor: Int, private val word: String) {
-    fun getWordOrEmpty(number: Int) =
-        if (number % divisor == 0) {
-            word
-        } else {
-            ""
-        }
+    fun getWordOrEmpty(number: Int) = if (number % divisor == 0) word else ""
 }
