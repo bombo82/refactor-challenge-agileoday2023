5bb30cc85fe56e590dbb10a3fe9f7476267dafa4
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-22 12:56:05 +0200

    injection della lista dal main

--- kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/Application.kt
+++ kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/Application.kt
@@ -1,7 +1,10 @@
 package it.giannibombelli.refactor_challenge
 
 fun main(args: Array<String>) {
-    val fizzBuzz = FizzBuzz()
+    val fizzBuzz = FizzBuzz(listOf(
+        DivisorAndWord(3, "Fizz"),
+        DivisorAndWord(5, "Buzz")
+    ))
     for (number in 1..100) {
         val say = fizzBuzz.say(number)
         println(say)
