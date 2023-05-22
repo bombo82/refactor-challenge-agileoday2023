f78f805200a3507d1d6f47ee9ffd43db6c7844f2
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-22 12:51:39 +0200

    reso privati gli attributi

--- kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/DivisorAndWord.kt
+++ kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/DivisorAndWord.kt
@@ -1,6 +1,6 @@
 package it.giannibombelli.refactor_challenge
 
-class DivisorAndWord(val divisor: Int, val word: String) {
+class DivisorAndWord(private val divisor: Int, private val word: String) {
     fun getWordOrEmpty(number: Int) =
         if (number % divisor == 0) {
             word
