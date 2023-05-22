4e8221ed849b09768f80118800ccb711c55cdabc
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-22 12:03:36 +0200

    reso evidente i dati

--- kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
+++ kotlin/src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
@@ -2,8 +2,8 @@ package it.giannibombelli.refactor_challenge
 
 class FizzBuzz : FizzBuzzInterface {
     override fun say(number: Int): String {
-        if (number % 15 == 0) {
-            return "FizzBuzz"
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "Fizz" + "Buzz"
         }
         if (number % 3 == 0) {
             return "Fizz"
