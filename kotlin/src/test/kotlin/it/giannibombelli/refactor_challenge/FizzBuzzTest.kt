5f58d60ede2acc6a4292e72ef2de405fe8b5fb6d
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2023-05-22 12:58:35 +0200

    injection della lista nei test unitari

--- kotlin/src/test/kotlin/it/giannibombelli/refactor_challenge/FizzBuzzTest.kt
+++ kotlin/src/test/kotlin/it/giannibombelli/refactor_challenge/FizzBuzzTest.kt
@@ -7,26 +7,38 @@ internal class FizzBuzzTest {
 
     @Test
     internal fun simpleNumber() {
-        val fizzBuzz = FizzBuzz()
+        val fizzBuzz = FizzBuzz(listOf(
+            DivisorAndWord(3, "Fizz"),
+            DivisorAndWord(5, "Buzz")
+        ))
         assertEquals("1", fizzBuzz.say(1))
         assertEquals("2", fizzBuzz.say(2))
     }
 
     @Test
     internal fun fizz() {
-        val fizzBuzz = FizzBuzz()
+        val fizzBuzz = FizzBuzz(listOf(
+            DivisorAndWord(3, "Fizz"),
+            DivisorAndWord(5, "Buzz")
+        ))
         assertEquals("Fizz", fizzBuzz.say(3))
     }
 
     @Test
     internal fun buzz() {
-        val fizzBuzz = FizzBuzz()
+        val fizzBuzz = FizzBuzz(listOf(
+            DivisorAndWord(3, "Fizz"),
+            DivisorAndWord(5, "Buzz")
+        ))
         assertEquals("Buzz", fizzBuzz.say(5))
     }
 
     @Test
     internal fun fizzBuzz() {
-        val fizzBuzz = FizzBuzz()
+        val fizzBuzz = FizzBuzz(listOf(
+            DivisorAndWord(3, "Fizz"),
+            DivisorAndWord(5, "Buzz")
+        ))
         assertEquals("FizzBuzz", fizzBuzz.say(15))
     }
 }
